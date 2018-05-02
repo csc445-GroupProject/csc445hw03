806387ae37537a921d4f330b1e0930003c2428e3
Stephen DiCerce <sdicerce@oswego.edu>
2018-05-02 18:34:31 -0400

    started on clientside GUI and started Client engine functionality

---
+++ src/main/java/clientpackage/Client.java
@@ -0,0 +1,46 @@
+package clientpackage;
+
+import packetpackage.SyncPacket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    private Socket connectionSocket;
+    private Socket communicationSocket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String serverName;
+    private int port;
+    private String userName;
+
+    Client(String userName){
+        userName = this.userName;
+        serverName = "pi";
+        port = 2706;
+        try {
+            connectionSocket = new Socket(serverName, port);
+        } catch(IOException e) {
+            System.out.println("Socket could Not be opened");
+        }
+        try {
+            in = new DataInputStream(connectionSocket.getInputStream());
+        } catch (IOException e) {
+            System.out.println("Input Stream was not instantiated");
+        }
+    }
+
+    /**
+     * This method creates the initial connection with the Server.
+     * @return
+     */
+    public boolean initialConnection() {
+        SyncPacket connectionPacket = new SyncPacket(SyncPacket.Opcode.JOIN, 0, System.nanoTime());
+        if(userName != null) {
+            connectionPacket.setPayload(userName.getBytes());
+        }
+        return false;
+    }
+}
---
+++ src/main/java/clientpackage/Main.java
@@ -0,0 +1,106 @@
+package clientpackage;
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+Client client;
+
+    public static void main(String args[]) {
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        LoginScreen(primaryStage);
+    }
+
+    /**
+     * This method Produces the login screen of the GUI
+     * @param primaryStage
+     */
+    public void LoginScreen(Stage primaryStage) {
+        primaryStage.setTitle("CSC 445 Playlist Syncing Application Login");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Button btn = new Button("Connect to Server");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                client = new Client();
+                if(client.initialConnection()){
+                    dashboard(primaryStage);
+                } else {
+                    actiontarget.setFill(Color.FIREBRICK);
+                    actiontarget.setText("Connection Unsuccessful");
+                }
+            }
+        });
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void dashboard(Stage primaryStage) {
+        primaryStage.setTitle("CSC 445 Playlist Syncing Application Dashboard");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Choose a Room");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+
+        Scene scene = new Scene(grid, 800, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
