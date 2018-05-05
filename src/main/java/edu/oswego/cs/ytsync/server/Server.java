5032e2abb2aef2296d1d99897d3dfd634da057e5
Stephen Iskander <siskande@oswego.edu>
2018-05-05 11:32:00 -0400

    verbose server option

--- src/main/java/edu/oswego/cs/ytsync/server/Server.java
+++ src/main/java/edu/oswego/cs/ytsync/server/Server.java
@@ -9,23 +9,33 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import edu.oswego.cs.ytsync.common.*;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class Server {
-
+	private static boolean verbose;
 	private final static int port = 2706;
 	private static List<String> videoQueue;
 
 	public static void main(String args[]) throws IOException {
+		verbose = ArrayUtils.contains(args, "-verbose");
 
 		ServerSocket host = new ServerSocket(port);
+		if(verbose) {
+			System.out.printf("Bound on port %d.\n", host.getLocalPort());
+		}
 		int clients = 0;
 		int rooms = 0;
 		long timestamp;
 		videoQueue = new ArrayList<>();
 		while (true) {
 			//Client first connects to the server
-			Socket client = host.accept(); 
+			Socket client = host.accept();
+			if(verbose) {
+				System.out.printf("New client %s:%d\n", client.getInetAddress().getHostAddress(), client.getPort());
+			}
 			//Open up a room with potential for more rooms later
 			if(rooms == 0) {
 				rooms += 1;
