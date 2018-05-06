a7ce31c0d3acbcfe29d4b14d689a4e19bff351d7
Stephen Iskander <siskande@oswego.edu>
2018-05-06 15:38:23 -0400

    broken packet stream

--- src/main/java/edu/oswego/cs/ytsync/common/PacketStream.java
+++ src/main/java/edu/oswego/cs/ytsync/common/PacketStream.java
@@ -9,33 +9,40 @@ import java.util.ArrayDeque;
 public class PacketStream {
     private ArrayDeque<Packet> bufferedPackets;
     private DataInputStream in;
+    private byte[] buff;
     private ByteBuffer buffer;
+    private int index;
 
     public PacketStream(DataInputStream in) {
         this.in = in;
-        buffer = ByteBuffer.allocate(32768);
+        buff = new byte[32768];
+        buffer = ByteBuffer.wrap(buff);
         bufferedPackets = new ArrayDeque<>();
     }
 
-    public void bufferPackets() throws IOException {
-        byte[] readBytes = new byte[16384];
-        int remaining = in.read(readBytes);
-        buffer.put(readBytes);
-        while (remaining > 4) {
-            int size = buffer.getInt(0);
-            if (buffer.remaining() >= size) {
-                buffer.position(0);
-                byte[] packetArray = new byte[size];
-                buffer.get(packetArray);
-                buffer.compact();
-                buffer.position(0);
-                remaining -= size;
+    public void bufferPackets() {
+        try {
+            if(in.available() > 0) {
+                int remaining = in.read(buff, buffer.position(), buff.length - buffer.position());
+                while (remaining > 4) {
+                    int size = buffer.getInt(0);
+                    if (remaining >= size) {
+                        buffer.position(0);
+                        byte[] packetArray = new byte[size];
+                        buffer.get(packetArray);
+                        buffer.compact();
+                        buffer.position(0);
+                        remaining -= size;
 
-                bufferedPackets.add(Packet.fromByteArray(packetArray));
-            } else {
-                buffer.position(remaining);
-                break;
+                        bufferedPackets.add(Packet.fromByteArray(packetArray));
+                    } else {
+                        buffer.position(remaining);
+                        break;
+                    }
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
--- src/test/java/edu/oswego/cs/ytsync/common/PacketStreamTest.java
+++ src/test/java/edu/oswego/cs/ytsync/common/PacketStreamTest.java
@@ -17,25 +17,41 @@ import static org.junit.Assert.*;
 public class PacketStreamTest {
 
     @Test
-    public void testStream() throws IOException {
+    public void testStream() throws IOException, InterruptedException {
         ServerSocket server = new ServerSocket(0);
         Socket client = new Socket(server.getInetAddress(), server.getLocalPort());
         client.setSoTimeout(500);
         Socket host = server.accept();
 
         List<Packet> sendPackets = new ArrayList<>();
-        for(int i = 0; i < 4; i++) {
-            sendPackets.add(new Packet(Opcode.CONNECT, System.nanoTime(), "hello".getBytes()));
-            host.getOutputStream().write(sendPackets.get(i).toByteArray());
-            host.getOutputStream().flush();
-        }
+        Thread sendThread = new Thread(() -> {
+            try {
+                for (int i = 0; i < 10000; i++) {
+                    sendPackets.add(new Packet(Opcode.CONNECT, System.nanoTime(), "hello".getBytes()));
+                    host.getOutputStream().write(sendPackets.get(i).toByteArray());
+                    host.getOutputStream().flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
         List<Packet> recvPackets = new ArrayList<>();
         PacketStream ps = new PacketStream(new DataInputStream(client.getInputStream()));
-        ps.bufferPackets();
-        for(int i = 0; i < 4; i++) {
-            recvPackets.add(ps.next());
-        }
+        Thread recvThread = new Thread(() -> {
+           for(int i = 0; i < 10000; i++) {
+               while (!ps.hasNext())
+                   ps.bufferPackets();
+
+               recvPackets.add(ps.next());
+           }
+        });
+
+        sendThread.start();
+        recvThread.start();
+        sendThread.join();
+        recvThread.join();
+
 
         assertEquals(sendPackets, recvPackets);
     }
