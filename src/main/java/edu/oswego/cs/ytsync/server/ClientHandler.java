1e22ac479e2539b62e6787ee36da3899de982753
Chazzle Dazzle <cmatthew@oswego.edu>
2018-05-09 04:13:55 -0400

    updated threading, packet handling, chat and queue lists

--- src/main/java/edu/oswego/cs/ytsync/server/ClientHandler.java
+++ src/main/java/edu/oswego/cs/ytsync/server/ClientHandler.java
@@ -8,6 +8,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.sql.PseudoColumnUsage;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class ClientHandler implements Runnable {
 	final DataOutputStream dout;
@@ -15,13 +17,17 @@ public class ClientHandler implements Runnable {
 	final Socket socket;
 	//final Client client;
 	final PacketStream ps;
+	final ConcurrentLinkedQueue<String> clq;
+	final PriorityBlockingQueue<String> pbq;
 
-	public ClientHandler(Socket socket) throws IOException{
+	public ClientHandler(Socket socket, ConcurrentLinkedQueue<String> clq, PriorityBlockingQueue<String> pbq) throws IOException{
 		//this.client = client;
 		this.socket = socket;
 		this.din = new DataInputStream(socket.getInputStream());
 		this.dout = new DataOutputStream(socket.getOutputStream());
 		this.ps = new PacketStream(din);
+		this.clq = clq;
+		this.pbq = pbq;
 	}
 	
 	public DataInputStream getDataInputStream() {
@@ -39,34 +45,31 @@ public class ClientHandler implements Runnable {
 
 	@Override
 	public void run() {
-		//client connects, exchange ConnectPacket
-		//First recieve
 
 		while (true) {
-			System.out.println("ayaya");
-			byte[] packetBuffer = new byte[2400];
-			try {
-				din.read(packetBuffer);
-			} catch(IOException e) {
-				System.out.println("Cant read buffer: total" + (10000 - packetBuffer.length));
-			}
-			//recieve, decode, and read packet from client
-			Packet request = Packet.fromByteArray(packetBuffer);
+
+			while(!ps.hasNext()) {
+                ps.bufferPackets();
+            } 
+            Packet request = ps.next();
 			Opcode operation = request.getOp();
-			System.out.println(operation);
+			
 			switch(operation) {
 				case CONNECT: {
+					if(request.getPayload() == null) {
+						request.setPayload(Thread.currentThread().getName().getBytes());
+					}
 					ConnectPacket rq = new ConnectPacket(request);
 					//ConnectPacket response = new ConnectPacket(System.currentTimeMillis(),Thread.currentThread().getName());
                     System.out.println(operation + " Connected: " + rq.getUsername());
-                    //need user info added to payload
-                    //Add username to list of clients
+                    
                     break;
                 }
 				case ADD_QUEUE: {
                     System.out.println(operation);
 					AddQueuePacket addQueuePacket = new AddQueuePacket(request);
 					String url = addQueuePacket.getId();
+					pbq.add(url);
                     break;
                 }
 				case REMOVE_QUEUE: {
@@ -78,7 +81,14 @@ public class ClientHandler implements Runnable {
 					break;
 				}
 				case CHAT: {
-					
+					MessagePacket messagePacket = new MessagePacket(request);
+					String message = messagePacket.getMessage();
+					System.out.println(operation + " new message: " + message);
+					clq.add(message);
+					System.out.println(message);
+					while(clq.size() > 1000) {
+						System.out.println("removed: " + clq.remove());
+					}
 					break;
 				}
 			}
--- src/main/java/edu/oswego/cs/ytsync/server/Server.java
+++ src/main/java/edu/oswego/cs/ytsync/server/Server.java
@@ -7,6 +7,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 
 import edu.oswego.cs.ytsync.Client;
 import edu.oswego.cs.ytsync.common.*;
@@ -17,8 +19,14 @@ public class Server extends Thread{
 	public static void main(String args[]) throws IOException {
 		ServerSocket host = new ServerSocket(PORT);
 		System.out.println("Listening on PORT %d.\n" + host.getLocalPort() + " address: " + host.getInetAddress()	);
+		//List of messages
+		ConcurrentLinkedQueue<String> clq = new ConcurrentLinkedQueue<>();
+		PriorityBlockingQueue<String> pbq = new PriorityBlockingQueue<>();
+		List<String> queueList = (List)pbq;
+		Thread[] allThreads;
 		
-		int x = 0;
+		
+		int x = 0; //possibly use to name threads
 		while (true) {
 			x += 1;
 			//Client first connects to the server
@@ -26,12 +34,13 @@ public class Server extends Thread{
 			System.out.printf("New connection from %s:%d\n", clientSocket.getInetAddress().getHostAddress(), clientSocket.getPort());
 				try {
 						//System.out.println("Thread");
-						ClientHandler clientHandler = new ClientHandler(clientSocket);
+						ClientHandler clientHandler = new ClientHandler(clientSocket, clq, pbq);
 						new Thread(clientHandler).run();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+				//Need to create way to send packets to all clients
 				}
 		}
 
