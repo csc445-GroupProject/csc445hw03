dcceba171dbf9721b7b0dffad75fded52fdb1543
Stephen Iskander <siskande@oswego.edu>
2018-05-11 13:36:49 -0400

    ui

--- src/main/java/edu/oswego/cs/ytsync/client/ClientApp.java
+++ src/main/java/edu/oswego/cs/ytsync/client/ClientApp.java
@@ -6,6 +6,7 @@ import com.sapher.youtubedl.YoutubeDLException;
 import com.sapher.youtubedl.YoutubeDLRequest;
 import com.sapher.youtubedl.YoutubeDLResponse;
 import edu.oswego.cs.ytsync.client.components.ConnectDialog;
+import edu.oswego.cs.ytsync.common.ConnectPacket;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -17,19 +18,16 @@ import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import java.io.IOException;
+import java.net.Socket;
 import java.util.List;
+import java.util.Map;
 
 public class ClientApp extends Application {
-    private Client client;
-
-    //private WebView ytWebView;
     private TextField chatField;
     private TextArea chatArea;
-    private TextField queueField;
-    private ListView<String> queueView;
-    MediaPlayer player;
-    MediaView playerView;
-    private Slider volumeSlider;
+    private ChatServer chatServer;
+    String username;
+    Socket server;
 
     public static void main(String args[]) {
         launch(args);
@@ -37,167 +35,31 @@ public class ClientApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-//        ytWebView = new WebView();
-//        ytWebView.managedProperty().bind(ytWebView.visibleProperty());
-//        ytWebView.setOnMouseClicked(me -> {
-//            System.out.println(ytWebView.getEngine().executeScript("player.getCurrentTime()"));
-//        });
-//        ytWebView.setPrefWidth(644);
-//        ytWebView.setPrefHeight(394);
-//        ytWebView.getEngine().load(this.getClass().getClassLoader().getResource("html/ytembed.html").toString());
+        new ConnectDialog().showAndWait().ifPresent(m -> {
+            username = m.get("username");
+            String serverName = m.get("server");
 
-        playerView = new MediaView();
-        playerView.managedProperty().bind(playerView.visibleProperty());
-        HBox.setHgrow(playerView, Priority.ALWAYS);
+            int port = Integer.parseInt(m.get("port"));
 
-        VBox root = new VBox();
-
-        VBox uiBox = new VBox();
-        VBox.setVgrow(uiBox, Priority.ALWAYS);
-        uiBox.setPadding(new Insets(20, 20, 20, 20));
-        uiBox.setSpacing(10);
-        playerView.fitHeightProperty().bind(root.heightProperty().multiply(.50));
-
-        //menu
-        MenuBar menuBar = new MenuBar();
-        Menu viewMenu = new Menu("View");
-        CheckMenuItem toggleVideo = new CheckMenuItem("Show Video");
-        toggleVideo.setSelected(true);
-        viewMenu.getItems().addAll(toggleVideo);
-        menuBar.getMenus().addAll(viewMenu);
-        root.getChildren().add(menuBar);
-
-        playerView.visibleProperty().bind(toggleVideo.selectedProperty());
-
-        HBox upper = new HBox();
-        upper.setSpacing(10);
-
-        VBox chatBox = new VBox();
-        chatBox.setSpacing(5);
-        VBox.setVgrow(chatBox, Priority.ALWAYS);
-
-        HBox chatInputBox = new HBox();
-        chatInputBox.setSpacing(5);
-
-        VBox queueBox = new VBox();
-        queueBox.setSpacing(5);
-        HBox.setHgrow(queueBox, Priority.ALWAYS);
-        //queueBox.prefHeightProperty().bind(playerView.fitHeightProperty());
-
-        HBox queueInputBox = new HBox();
-        queueInputBox.setSpacing(5);
-
-        chatField = new TextField();
-        HBox.setHgrow(chatField, Priority.ALWAYS);
-        Button sendButton = new Button("Send");
-        chatInputBox.getChildren().addAll(chatField, sendButton);
-
-        chatArea = new TextArea();
-        chatArea.setEditable(false);
-        chatArea.appendText("Welcome to chat!\n\n");
-        VBox.setVgrow(chatArea, Priority.ALWAYS);
-        chatBox.getChildren().addAll(chatArea, chatInputBox);
-
-        queueField = new TextField();
-        queueField.setPromptText("Enter a YouTube URL");
-        HBox.setHgrow(queueField, Priority.ALWAYS);
-        Button submitButton = new Button("Add");
-        queueInputBox.getChildren().addAll(queueField, submitButton);
-
-        queueView = new ListView<>();
-        VBox.setVgrow(queueView, Priority.ALWAYS);
-        queueBox.getChildren().addAll(queueInputBox, queueView);
-        upper.getChildren().addAll(playerView, queueBox);
-
-        uiBox.getChildren().addAll(upper, chatBox);
-        root.getChildren().add(uiBox);
-
-        Scene scene = new Scene(root, 960, 720);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("YTSync");
-        primaryStage.show();
-        //player.play();
-        ConnectDialog connectDialog = new ConnectDialog();
-        connectDialog.showAndWait().ifPresent(dialogMap -> {
-            String username = dialogMap.get("username");
-            String server = dialogMap.get("server");
-            int port = Integer.parseInt(dialogMap.get("port"));
             try {
-                client = new Client(server, port, username);
-                new Thread(client).start();
+                server = new Socket(serverName, port);
             } catch (IOException e) {
-                Alert errAlert = new Alert(Alert.AlertType.ERROR, "Could not connect to server.");
-                errAlert.showAndWait();
+                throw new RuntimeException();
             }
-            while(true) {
-                int listenInt = client.listen();
-                if(listenInt == 0) {
-                    List<String> queue = client.sendQueueToGUI();
-                } else if(listenInt == 1) {
-                    List<String> chatList = client.sendChatToGUI();
-                }
-            }
+
+            chatField = new TextField();
+            chatArea = new TextArea();
+            VBox.setVgrow(chatArea, Priority.ALWAYS);
+            chatArea.setEditable(false);
+            HBox.setHgrow(chatField, Priority.ALWAYS);
+            Button sendButton = new Button("Send");
+            HBox inputBox = new HBox(10, chatField, sendButton);
+
+            VBox root = new VBox(10, chatArea, inputBox);
+            root.setPadding(new Insets(10, 10, 10, 10));
+            primaryStage.setScene(new Scene(root));
+            chatField.requestFocus();
+            primaryStage.show();
         });
-
-        sendButton.setOnAction(e -> {
-            handleInput();
-        });
-
-        chatField.setOnKeyPressed(ke -> {
-            switch (ke.getCode()) {
-                case ENTER: {
-                    handleInput();
-                }
-            }
-        });
-
-        submitButton.setOnAction(e -> {
-            YoutubeDLRequest request = new YoutubeDLRequest(queueField.getText().trim());
-            request.setOption("format", "mp4");
-            request.setOption("get-url");
-            try {
-                if(player != null)
-                    player.stop();
-                YoutubeDLResponse response = YoutubeDL.execute(request);
-                String url = response.getOut().trim();
-                if(player != null)
-                    player.dispose();
-                player = new MediaPlayer(new Media(url));
-                playerView.setMediaPlayer(player);
-                player.play();
-                player.setVolume(.5);
-                chatArea.appendText("The Volume of this Video is: " + player.getVolume() + ".\n\n");
-            } catch (YoutubeDLException e1) {
-                e1.printStackTrace();
-            }
-        });
-    }
-
-    private void handleInput() {
-        // seeks the video the number entered is the number of seconds the video will sync to from the beginning of the song
-        if(chatField.getText().startsWith("!seek ")) {
-            double offset = Double.parseDouble(chatField.getText().substring(6));
-            player.seek(Duration.seconds(offset));
-        }
-
-        //toggles mute for the video
-        if(chatField.getText().startsWith("!mute")) {
-            if(player.isMute()) {
-                player.setMute(false);
-            } else {
-                player.setMute(true);
-            }
-        }
-
-        //controls the volume
-        if(chatField.getText().startsWith("!volume ")) {
-            double volume = Double.parseDouble(chatField.getText().substring(8));
-            if(volume >= 0 && volume <= 1.0)
-                player.setVolume(volume);
-        }
-
-
-        chatArea.appendText(String.format("User: %s\n", /*client.getUsername(), */chatField.getText().trim()));
-        chatField.clear();
     }
 }
